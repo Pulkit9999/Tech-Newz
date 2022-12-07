0544d34b46d6cd5a70248b62050c7cfda672adb8
unknown <pullkit.9@gmail.com>
2022-12-07 21:28:07 +0530

    Complete Tech Newz App

---
+++ android/app/src/main/kotlin/com/example/tech_newz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tech_newz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/key.dart
@@ -0,0 +1,3 @@
+class Key {
+  static String key = 'a3f01446a1ae401fb601f97958640152';
+}
